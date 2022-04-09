c38b5cd9ed2d8746f77bc390367beaa721094a91
anand0202 <102712272+anand0202@users.noreply.github.com>
2022-04-09 14:24:03 +0530

    Auth0 basic page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
+import LogoutButton from './logout';
+import LoginButton from './login';
+import Profile from './profile';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"style={{margin:20, display:'flex', flexDirection:'column', maxWidth:200,alignItems:'center' }} >
+
+     <LoginButton />
+     <LogoutButton />
+      <Profile />
     </div>
   );
 }
